30f7dac840490c15eb26d31cd4d1c42a466c54b0
Abhishek Sharma <sharmabhishek802158@gmail.com>
2024-08-07 13:22:56 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold  bg-orange-600">
+      Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
