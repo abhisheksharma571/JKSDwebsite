75077500b1484d3750f6befc1a6da59a1b4eaf83
Ayush <shakyaayush31@gmail.com>
2024-08-28 22:44:02 +0530

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,8 @@ import './App.css'
 
 import Nav from './components/Nav'
 import Foot from './components/Foot'
-<<<<<<< HEAD
-import Review from './components/Review'
-import ImageSlider from './components/ImageSlider'
-import Services from './components/Services'
-import Cources from './components/Cources'
-import Biocourses from './components/Biocourses'
-=======
+
 import { Outlet, useLocation } from 'react-router-dom'
-
->>>>>>> 47fcf3aa30a88daf71d9dc413d996da569087fcd
-
-
 
 
 function App() {
@@ -24,19 +14,6 @@ function App() {
   return (
     <>
       <div className='bg-backgound'>
-<<<<<<< HEAD
-        <Nav />
-        <Biocourses/>
-        <Cources />
-        <ImageSlider />
-        <Type />
-        <Services />
-        <SelectedStudents />
-        <Review />
-        <HiringPertners />
-        <Foot />
-=======
->>>>>>> 47fcf3aa30a88daf71d9dc413d996da569087fcd
 
       {!isNotFoundPage && <Nav />}
       <Outlet />
