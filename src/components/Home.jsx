419d66c297f2d18f413beeaa7b7eccd7246c3305
rajabhishek5041 <rajabhishek5041@gmail.com>
2024-09-09 13:44:25 +0530

    Services Page Completed2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import ImageSlider from './ImageSlider'
 import CounterWrapper from './CounterWrapper'
-import Services from './Services'
 import TrendingCourses from './TrendingCourses'
 import Review from './Review'
 import HiringPartners from './HiringPartners'
 import Type from './Type'
 import Adviser from './Adviser'
-
-import Biocourses from './Biocourses'
-
-
+import Services from './Services'
 
 function Home() {
   return (
@@ -20,13 +16,12 @@ function Home() {
       <Type />
       <CounterWrapper />
       <Services />
-      <div className='bg-indigo-950'>
       <TrendingCourses/>
-      </div>
       <Review/>
+
       <HiringPartners />
       <Adviser />
-      <Biocourses/>
+
       </div>
     </div>
   )
